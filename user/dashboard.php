fafef28b7c333b318768ec895e12de4b13fdcf4d
unknown <kaykay76541@gmail.com>
2023-05-22 23:16:25 -0500

    Added the user dashboard

---
+++ user/dashboard.php
@@ -0,0 +1,82 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="bootstrap/css/bootstrap.css">
+    <link rel="stylesheet" href="font-awesome/fonts/css/all.css">
+    <link rel="stylesheet" href="dashboard.css">
+</head>
+
+<body>
+    <div class="container">
+        <div class="sub-container">
+            <div class="header">
+                <h1>Welcome To Venor Mall</h1>
+                <p>Here are some tips to help you get started.</p>
+            </div>
+            <div class="content-container">
+                <div class="content">
+                    <div class="content-sub">
+                        <div class="desc-cont">
+                            <div class="desc-img-container">
+                                <img src="images/pic.jpg">
+                            </div>
+
+                            <div class="desc">
+                                <h3>Add Products</h3>
+                                <div class="sub-desc">
+                                    <p>Let's roll your products out!</p>
+                                    <p>Showcase your products with extreme descriptions.The more detailed,the better.</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="button-cont">
+                            <button class="btn btn-primary">Order</button>
+                        </div>
+                    </div>
+                    <div class="content-sub">
+                        <div class="desc-cont">
+                            <div class="desc-img-container">
+                                <img src="images/pic.jpg">
+                            </div>
+
+                            <div class="desc">
+                                <h3>Publish</h3>
+                                <div class="sub-desc">
+                                    <p>Register.set.publish</p>
+                                    <p>Our easy-to-follow site will walk you through ever step from domain mapping to publishing</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="button-cont">
+                            <button class="btn btn-primary">Order</button>
+                        </div>
+                    </div>
+                    <div class="content-sub">
+                        <div class="desc-cont">
+                            <div class="desc-img-container">
+                                <img src="images/pic.jpg">
+                            </div>
+
+                            <div class="desc">
+                                <h3>Taxes</h3>
+                                <div class="sub-desc">
+                                    <p>Lower Taxes</p>
+                                    <p>Configure your storw wide taxes</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="button-cont">
+                            <button class="btn btn-primary">Order</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+</body>
+
+</html>
