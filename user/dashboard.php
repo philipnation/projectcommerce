aec530a311ddda9a8e21b7e57dad21c4895d38e3
unknown <kaykay76541@gmail.com>
2023-05-24 23:25:45 -0500

    Added new features

--- user/dashboard.php
+++ user/dashboard.php
@@ -15,66 +15,68 @@ include('bars.php');
 </head>
 
 <body>
-    <div class="container">
-        <div class="sub-container">
-            <div class="header">
-                <h1>Welcome To Venor Mall</h1>
-                <p>Here are some tips to help you get started.</p>
-            </div>
-            <div class="content-container">
-                <div class="content">
-                    <div class="content-sub">
-                        <div class="desc-cont">
-                            <div class="desc-img-container">
-                                <img src="images/opt.svg">
-                            </div>
+    <div class="body">
+        <div class="container">
+            <div class="sub-container">
+                <div class="header">
+                    <h1>Welcome To Venor Mall</h1>
+                    <p>Here are some tips to help you get started.</p>
+                </div>
+                <div class="content-container">
+                    <div class="content">
+                        <div class="content-sub">
+                            <div class="desc-cont">
+                                <div class="desc-img-container">
+                                    <img src="images/opt.svg">
+                                </div>
 
-                            <div class="desc">
-                                <h3>Add Products</h3>
-                                <div class="sub-desc">
-                                    <p>Let's roll your products out!</p>
-                                    <p>Showcase your products with extreme descriptions.The more detailed,the better.</p>
+                                <div class="desc">
+                                    <h3>Add Products</h3>
+                                    <div class="sub-desc">
+                                        <p>Let's roll your products out!</p>
+                                        <p>Showcase your products with extreme descriptions.The more detailed,the better.</p>
+                                    </div>
                                 </div>
                             </div>
-                        </div>
-                        <div class="button-cont">
-                            <button class="btn btn-primary"><a href='add.php'>Add Products</a></button>
-                        </div>
-                    </div>
-                    <div class="content-sub">
-                        <div class="desc-cont">
-                            <div class="desc-img-container">
-                                <img src="images/content.svg">
+                            <div class="button-cont">
+                                <button class="btn btn-primary"><a href='add.php'>Add Products</a></button>
                             </div>
+                        </div>
+                        <div class="content-sub">
+                            <div class="desc-cont">
+                                <div class="desc-img-container">
+                                    <img src="images/content.svg">
+                                </div>
 
-                            <div class="desc">
-                                <h3>Publish</h3>
-                                <div class="sub-desc">
-                                    <p>Register.Set.Publish</p>
-                                    <p>Our easy-to-follow site will walk you through ever step from domain mapping to publishing</p>
+                                <div class="desc">
+                                    <h3>Publish</h3>
+                                    <div class="sub-desc">
+                                        <p>Register.Set.Publish</p>
+                                        <p>Our easy-to-follow site will walk you through ever step from domain mapping to publishing</p>
+                                    </div>
                                 </div>
                             </div>
-                        </div>
-                        <div class="button-cont">
-                            <button class="btn btn-primary"><a href='#'>Publish</a></button>
-                        </div>
-                    </div>
-                    <div class="content-sub">
-                        <div class="desc-cont">
-                            <div class="desc-img-container">
-                                <img src="images/seo.svg">
+                            <div class="button-cont">
+                                <button class="btn btn-primary"><a href='#'>Publish</a></button>
                             </div>
+                        </div>
+                        <div class="content-sub">
+                            <div class="desc-cont">
+                                <div class="desc-img-container">
+                                    <img src="images/seo.svg">
+                                </div>
 
-                            <div class="desc">
-                                <h3>Order</h3>
-                                <div class="sub-desc">
-                                    <p>Meet your needs</p>
-                                    <p>Configure your store wide taxes</p>
+                                <div class="desc">
+                                    <h3>Order</h3>
+                                    <div class="sub-desc">
+                                        <p>Meet your needs</p>
+                                        <p>Configure your store wide taxes</p>
+                                    </div>
                                 </div>
                             </div>
-                        </div>
-                        <div class="button-cont">
-                            <button class="btn btn-primary"><a href='#'>Order products</a></button>
+                            <div class="button-cont">
+                                <button class="btn btn-primary"><a href='#'>Order products</a></button>
+                            </div>
                         </div>
                     </div>
                 </div>
